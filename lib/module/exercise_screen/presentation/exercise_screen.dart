227cf4a04ef16378b764d5cc919d58e65d1329b9
Ashish Suvagiya <adsuvagiya07@gmail.com>
2023-08-15 19:06:21 +0530

    # set Flutter CachedNetworkImage

--- lib/module/exercise_screen/presentation/exercise_screen.dart
+++ lib/module/exercise_screen/presentation/exercise_screen.dart
@@ -10,98 +10,99 @@ class ExerciseScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: const SystemUiOverlayStyle(
-        statusBarIconBrightness: Brightness.light,
-        statusBarColor: Colors.transparent,
-        systemNavigationBarColor: Colors.transparent,
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.grey.withOpacity(0.4),
-        appBar: AppBar(
-          title: const Text("Fit Basix Demo"),
-          centerTitle: true,
-          elevation: 0.00,
-          backgroundColor: Colors.greenAccent[400],
+    return Scaffold(
+      backgroundColor: Colors.grey.withOpacity(0.4),
+      appBar: AppBar(
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.light,
+          statusBarBrightness: Brightness.light,
         ),
-        body: Obx(
-          () => exerciseController.isLoading.value
-              ? const Center(
-                  child: CircularProgressIndicator(
-                    color: Colors.black,
-                    strokeWidth: 1.5,
-                  ),
-                )
-              : ListView.builder(
-                  itemCount: exerciseController.allExercise.value.length,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 10),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white,
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(18),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Equipment : ${exerciseController.allExercise.value[index].equipment ?? ""}",
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
+        title: const Text("Fit Basix Demo"),
+        centerTitle: true,
+        elevation: 0.00,
+        backgroundColor: Colors.greenAccent[400],
+      ),
+      body: Obx(
+        () => exerciseController.isLoading.value
+            ? const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.black,
+                  strokeWidth: 1.5,
+                ),
+              )
+            : ListView.builder(
+                itemCount: exerciseController.allExercise.value.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(18),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Equipment : ${exerciseController.allExercise.value[index].equipment ?? ""}",
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Target : ${exerciseController.allExercise.value[index].target ?? ""}",
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            CachedNetworkImage(
+                              imageUrl: exerciseController
+                                      .allExercise.value[index].gifUrl ??
+                                  "",
+                              fit: BoxFit.cover,
+                              width: MediaQuery.of(context).size.width,
+                              placeholder: (context, url) => Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.withOpacity(0.1),
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  height: 200,
+                                  width: double.infinity,
                                 ),
                               ),
-                              Text(
-                                "Target : ${exerciseController.allExercise.value[index].target ?? ""}",
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                              errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
+                            ),
+                            Text(
+                              "Name : ${exerciseController.allExercise.value[index].name ?? ""}",
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
                               ),
-                              CachedNetworkImage(
-                                imageUrl: exerciseController.allExercise.value[index].gifUrl ?? "",
-                                fit: BoxFit.cover,
-                                width: MediaQuery.of(context).size.width,
-                                placeholder: (context,url) => CircularProgressIndicator(),
-                                errorWidget: (context,url,error) => new Icon(Icons.error),
+                            ),
+                            Text(
+                              "BodyPart : ${exerciseController.allExercise.value[index].bodyPart ?? ""}",
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
                               ),
-                              // Center(
-                              //   child: Image.network(
-                              //     exerciseController
-                              //             .allExercise.value[index].gifUrl ??
-                              //         "",
-                              //     width: 300,
-                              //     height: 200,
-                              //     fit: BoxFit.contain,
-                              //   ),
-                              // ),
-                              Text(
-                                "Name : ${exerciseController.allExercise.value[index].name ?? ""}",
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                "BodyPart : ${exerciseController.allExercise.value[index].bodyPart ?? ""}",
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ),
-                    );
-                  },
-                ),
-        ),
+                    ),
+                  );
+                },
+              ),
       ),
     );
   }
